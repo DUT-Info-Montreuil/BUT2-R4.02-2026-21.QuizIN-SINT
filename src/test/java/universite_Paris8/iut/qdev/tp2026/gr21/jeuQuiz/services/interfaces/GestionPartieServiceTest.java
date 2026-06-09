fd4abf659584b4290adef5d434688d049c10cdc0
Parchemal <nguimgoparchemal@gmail.com>
2026-06-09 12:33:54 +0200

    mockito test

--- src/test/java/universite_Paris8/iut/qdev/tp2026/gr21/jeuQuiz/services/interfaces/GestionPartieServiceTest.java
+++ src/test/java/universite_Paris8/iut/qdev/tp2026/gr21/jeuQuiz/services/interfaces/GestionPartieServiceTest.java
@@ -1,16 +1,19 @@
 package universite_Paris8.iut.qdev.tp2026.gr21.jeuQuiz.services.interfaces;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import universite_Paris8.iut.qdev.tp2026.gr21.jeuQuiz.entities.dtos.JoueurDispoDTO;
+import universite_Paris8.iut.qdev.tp2026.gr21.jeuQuizz.entities.dtos.JoueurDTO;
 import universite_Paris8.iut.qdev.tp2026.gr21.jeuQuizz.services.interfaces.IJoueurService;
+import universite_Paris8.iut.qdev.tp2026.gr21.jeuQuizz.utils.exceptions.AucunJoueurEnregistreException;
 import universite_paris8.iut.qdev.tp2026.gr21.jeuQuizz.services.interfaces.IQuestionnaireService;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GestionPartieServiceTest {
@@ -22,10 +25,16 @@ class GestionPartieServiceTest {
     @Mock
     IQuestionnaireService iQuestionnaireService;
 
-    GestionPartieService objetsATester;
+    GestionPartieService gestionPartieService;
 
     @Test
-    public void aucunJoueurException(){
+    public void aucunJoueurException() throws AucunJoueurEnregistreException {
+
+        JoueurDTO JoueurDTOAttendu = new JoueurDTO();
+        when(iJoueurService.listerJoueurs()).thenReturn(List.of(JoueurDTOAttendu));
+
+        assertThrows(AucunJoueurEnregistreException.class,() -> gestionPartieService.determinerElementsDispoPourPartie());
+
 
     }
 
